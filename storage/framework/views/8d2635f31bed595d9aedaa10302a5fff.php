9a2426c2b60d83be67cd9d86db3d745043ce68d8
JaJo EkiZ <jajoultrapunk@gmail.com>
2025-09-25 04:32:04 -0300

    deploy con docker

---
+++ storage/framework/views/8d2635f31bed595d9aedaa10302a5fff.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Conversor de dólares</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100">
+    <div class="container mx-auto py-8">
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\dolar-api\resources\views\layouts\app.blade.php ENDPATH**/ ?>
